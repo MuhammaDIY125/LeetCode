b630277f2a62f1f13745df5634210cb8801b81dc
Muhammadiy <163087296+MuhammaDIY125@users.noreply.github.com>
2024-03-18 23:01:36 +0500

    5 masala

--- 5_Longest_Palindromic_Substring.py
+++ 5_Longest_Palindromic_Substring.py
@@ -1,6 +1,13 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        lst = [s[0]]
+        while len(s) > 1:
+            for i in range(len(s), 1, -1):
+                j = s[:i]
+                if j == j[::-1]:
+                    lst.append(j)
+            s = s[1:]
+        return max(lst, key=lambda x: len(x))
 
-s = "babad"
+s = input(">>> ")
 print(Solution().longestPalindrome(s))
