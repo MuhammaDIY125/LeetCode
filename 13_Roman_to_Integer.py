9eb227091d99b35c2777085267a3fd3e872d2308
Muhammadiy <163087296+MuhammaDIY125@users.noreply.github.com>
2024-04-03 22:55:37 +0500

    qwert

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -1,25 +1,30 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        all_roman = [(1000, 'M'),
-                     (900, 'CM'),
-                     (500, 'D'),
-                     (400, 'CD'),
-                     (100, 'C'),
-                     (90, 'XC'),
-                     (50, 'L'),
-                     (40, 'XL'),
-                     (10, 'X'),
-                     (9, 'IX'),
-                     (5, 'V'),
-                     (4, 'IV'),
-                     (1, 'I')]
-        dec = 0
-        for i, r in all_roman:
+class RomanNumerals:
+    rim_sonlar = [(1000, 'M'),
+                  (900, 'CM'),
+                  (500, 'D'),
+                  (400, 'CD'),
+                  (100, 'C'),
+                  (90, 'XC'),
+                  (50, 'L'),
+                  (40, 'XL'),
+                  (10, 'X'),
+                  (9, 'IX'),
+                  (5, 'V'),
+                  (4, 'IV'),
+                  (1, 'I')]
+
+    @staticmethod
+    def to_roman(self, s: str) -> int:
+        son = 0
+        for i, r in self.rim_sonlar:
             while s.startswith(r):
-                dec += i
+                son += i
                 s = s[len(r):]
-        return dec
+        return son
+    
+    @staticmethod
+    def from_roman(roman_num : str) -> int:
+        return 0
 
-s1 = Solution()
-
-print("Число MMXXII в римской системе = " + str(s1.romanToInt("MMXXII")))
+s1 = RomanNumerals()
+print(s1.to_roman("MMXXII"))
